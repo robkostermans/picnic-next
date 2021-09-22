8e0b1fcc46811b9626171bc9f3a139ffc93b46f5
Rob Kostermans <rob.kostermans@gmail.com>
2021-09-22 23:50:59 +0200

    hello api

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,20 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const PicnicClient = require("picnic-api");
+const picnicClient = new PicnicClient();
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+const doPicnic = async () => {
+  await picnicClient.login(process.env.USERNAME, process.env.PASSWORD);
+  return await picnicClient.getUserDetails();
+
+  //let searchResults = await picnicClient.search("Affligem blond");
+  //console.log("User", login);
+  //console.log("User", myvar);
+  //console.log("searchresults", searchResults[0].items);
+
+  //await picnicClient.addProductToShoppingCart(11389996, 1);
+};
+
+export default async function handler(req, res) {
+  let userDetails = await doPicnic();
+  res.status(200).json(userDetails.firstname);
 }
